0420bf8e8d6e375072967f2085a731be94f9fbbf
Zahraa <zahraamirza3034@gmail.com>
2021-11-28 18:05:32 +0300

    Work on "Onboarding page"

--- lib/global/colors.dart
+++ lib/global/colors.dart
@@ -5,5 +5,9 @@ class AppColors {
   // static const Color sampleHex = Color(0xFF253745);
   //--------------------------------------
   // Add your custom colors below
-  
+
+  static const Color blue8DC4CB = Color(0xFF8DC4CB);
+  static const Color greyB9BEC2 = Color(0xFFB9BEC2);
+  static const Color greyF2F3F3 = Color(0xFFF2F3F3);
+  static const Color black45515D = Color(0xFF45515D);
 }
