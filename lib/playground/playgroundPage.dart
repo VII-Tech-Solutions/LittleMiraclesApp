fedb49e40ee539eda24e70484128e91fcdb72485
Zahraa <zahraamirza3034@gmail.com>
2021-12-02 14:17:16 +0300

    Playground code cleanup

--- lib/playground/playgroundPage.dart
+++ lib/playground/playgroundPage.dart
@@ -1,17 +1,10 @@
 //PACKAGES
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 //GLOBAL
 //MODELS
 //PROVIDERS
-import '../../providers/appData.dart';
 //WIDGETS
 import '../widgets/texts/titleText.dart';
-import '../widgets/containers/workshopContainer.dart';
-import '../widgets/containers/promotionContainer.dart';
-import '../widgets/containers/actionContainer.dart';
-import '../widgets/containers/tipContainer.dart';
-//PAGES
 
 class PlayrgoundPage extends StatefulWidget {
   const PlayrgoundPage({Key? key}) : super(key: key);
