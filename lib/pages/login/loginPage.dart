e0d98f65edfe27c07db03025cbd75985d9669575
Ahmed Ebrahim <ahmed.ebrahim@viitech.net>
2021-12-06 15:56:09 +0300

    login page design fix

--- lib/pages/login/loginPage.dart
+++ lib/pages/login/loginPage.dart
@@ -9,6 +9,7 @@ import '../../global/colors.dart';
 //WIDGETS
 import '../../widgets/texts/titleText.dart';
 import '../../widgets/buttons/buttonWithIconWidget.dart';
+import '../../widgets/buttons/iconButtonWidget.dart';
 //PAGES
 import '../../pages/login/completeProfilePage.dart';
 
@@ -26,129 +27,134 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.white.withOpacity(0.0),
+        shadowColor: Colors.transparent,
+        toolbarHeight: 66,
+        leadingWidth: 66,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: IconButtonWidget(
+              onPress: () {
+                Navigator.maybePop(context);
+              },
+              icon: Icons.close),
+        ),
+      ),
       body: Stack(
-        alignment: Alignment.bottomCenter,
         children: [
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            child: Image.asset(
-              'assets/images/login_bg.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  AppColors.pinkF8C0B8.withOpacity(0.0),
-                  AppColors.pinkF8C0B8,
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 40.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                MaterialButton(
-                  elevation: 0,
-                  onPressed: () {
-                    Navigator.maybePop(context);
-                  },
-                  color: AppColors.greyF2F3F3,
-                  child: Icon(
-                    Icons.clear_rounded,
-                    color: AppColors.black45515D,
-                    size: 24,
-                  ),
-                  padding: EdgeInsets.all(8.0),
-                  shape: CircleBorder(),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          Column(
+            children: [
+              Expanded(
+                child: Stack(
                   children: [
-                    TitleText(
-                      title:
-                          'Sign in to unlock the best of Little Miracles by Sherin.',
+                    Image.asset(
+                      'assets/images/login_bg.png',
+                      height: double.infinity,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 6.0),
-                      child: Text.rich(
-                        TextSpan(
-                          text: 'By proceeding, you agree to our ',
-                          style: TextStyle(
-                            color: AppColors.black45515D,
-                          ),
-                          children: <InlineSpan>[
-                            TextSpan(
-                              text: 'Terms of Use ',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontWeight: FontWeight.w700,
-                              ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  //todo go to Terms of Use
-                                  _launchURL(
-                                      'https://www.littlemiraclesbys.com/about/');
-                                },
-                            ),
-                            TextSpan(
-                              text: 'and confirm you have read our ',
-                            ),
-                            TextSpan(
-                              text: 'Privacy and Cookie Statement',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontWeight: FontWeight.w700,
-                              ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  //todo go to Privacy and Cookie Statement
-                                  _launchURL(
-                                      'https://www.littlemiraclesbys.com/faq/');
-                                },
-                            ),
-                            TextSpan(
-                              text: '.',
-                            ),
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [0, 0.73, 1],
+                          colors: [
+                            AppColors.pinkF8C0B8.withOpacity(0.0),
+                            AppColors.pinkF8C0B8.withOpacity(0.5),
+                            AppColors.pinkF8C0B8,
                           ],
                         ),
                       ),
                     ),
-                    ButtonWithIconWidget(
-                      buttonText: 'Login using Google',
-                      onPress: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => CompleteProfilePage(),
-                          ),
-                        );
-                      },
-                      assetName: 'assets/images/iconsSocialGoogle.svg',
-                    ),
-                    ButtonWithIconWidget(
-                      buttonText: 'Login using Facebook',
-                      onPress: () {},
-                      assetName: 'assets/images/iconsSocialFacebook.svg',
-                    ),
-                    ButtonWithIconWidget(
-                      buttonText: 'Login using Snapchat',
-                      onPress: () {},
-                      assetName: 'assets/images/iconsSocialSnapchat.svg',
-                    ),
                   ],
                 ),
-              ],
-            ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.326,
+                color: AppColors.pinkF8C0B8,
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TitleText(
+                title:
+                    'Sign in to unlock the best of Little Miracles by Sherin.',
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 6.0),
+                child: Text.rich(
+                  TextSpan(
+                    text: 'By proceeding, you agree to our ',
+                    style: TextStyle(
+                      color: AppColors.black45515D,
+                    ),
+                    children: <InlineSpan>[
+                      TextSpan(
+                        text: 'Terms of Use ',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.w700,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            //todo go to Terms of Use
+                            _launchURL(
+                                'https://www.littlemiraclesbys.com/about/');
+                          },
+                      ),
+                      TextSpan(
+                        text: 'and confirm you have read our ',
+                      ),
+                      TextSpan(
+                        text: 'Privacy and Cookie Statement',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.w700,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            //todo go to Privacy and Cookie Statement
+                            _launchURL(
+                                'https://www.littlemiraclesbys.com/faq/');
+                          },
+                      ),
+                      TextSpan(
+                        text: '.',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ButtonWithIconWidget(
+                onPress: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CompleteProfilePage(),
+                    ),
+                  );
+                },
+                buttonText: 'Login using Google',
+                assetName: 'assets/images/iconsSocialGoogle.svg',
+              ),
+              ButtonWithIconWidget(
+                buttonText: 'Login using Facebook',
+                onPress: () {},
+                assetName: 'assets/images/iconsSocialFacebook.svg',
+              ),
+              ButtonWithIconWidget(
+                buttonText: 'Login using Snapchat',
+                onPress: () {},
+                assetName: 'assets/images/iconsSocialSnapchat.svg',
+              ),
+              SizedBox(height: 24),
+            ],
           ),
         ],
       ),
