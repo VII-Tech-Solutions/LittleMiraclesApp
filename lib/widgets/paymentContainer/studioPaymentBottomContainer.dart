63860c250014cc4596ef47ac57905e31e7de3d7c
Hamza <hamza@myspringring.com>
2022-02-16 17:46:56 +0300

    shopping cart page half way done, payment yet to be added, provider functions are dummy, no API integration yet

---
+++ lib/widgets/paymentContainer/studioPaymentBottomContainer.dart
@@ -0,0 +1,71 @@
+//PACKAGES
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+//GLOBAL
+import '../../global/colors.dart';
+//MODELS
+//PROVIDERS
+import '../../providers/studio.dart';
+//WIDGETS
+import '../../widgets/buttons/filledButtonWidget.dart';
+//PAGES
+
+class StudioPaymentBottomContainer extends StatelessWidget {
+  final VoidCallback? onTapCallback;
+  const StudioPaymentBottomContainer({@required this.onTapCallback});
+
+  @override
+  Widget build(BuildContext context) {
+    // final package = context.watch<Studio>().package;
+    // final promoCode = context.watch<Studio>().promoCode;
+
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      // height: promoCode?.code == null ? 100 : 123,
+      height: 100,
+      width: double.infinity,
+      color: AppColors.greyF2F3F3,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Visibility(
+          //   visible: promoCode?.code != null,
+          //   child: Text(
+          //     '- BD ${promoCode?.discountPrice ?? ''}',
+          //     style: TextStyle(
+          //       color: AppColors.redED0006,
+          //       fontSize: 16,
+          //       fontWeight: FontWeight.w500,
+          //     ),
+          //   ),
+          // ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Text(
+                  'BD 349.65',
+                  // 'BD ${promoCode?.totalPrice ?? package?.price ?? ''}',
+                  style: TextStyle(
+                    color: AppColors.black45515D,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+              FilledButtonWidget(
+                customWidth: 200,
+                onPress: onTapCallback,
+                type: ButtonType.generalBlue,
+                title: 'Continue to Checkout',
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/paymentContainer/studioPromoCodeContainer.dart
@@ -0,0 +1,161 @@
+//PACKAGES
+import 'package:LMP0001_LittleMiraclesApp/providers/studio.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+//EXTENSIONS
+//GLOBAL
+import '../../global/colors.dart';
+import '../../global/const.dart';
+//MODELS
+//PROVIDERS
+import '../../providers/bookings.dart';
+//WIDGETS
+import '../../widgets/buttons/filledButtonWidget.dart';
+import '../../widgets/dialogs/showOkDialog.dart';
+import '../../widgets/dialogs/showLoadingDialog.dart';
+//PAGES
+
+class StudioPromoCodeContainer extends StatefulWidget {
+  const StudioPromoCodeContainer();
+
+  @override
+  State<StudioPromoCodeContainer> createState() =>
+      _StudioPromoCodeContainerState();
+}
+
+class _StudioPromoCodeContainerState extends State<StudioPromoCodeContainer> {
+  bool flip = false;
+  var code = '';
+  late final _textFieldController;
+
+  @override
+  void initState() {
+    _textFieldController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textFieldController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final promoCode = context.watch<Studio>().promoCode;
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 5.0),
+            child: Text(
+              'Promo Code',
+              style: TextStyle(
+                fontSize: 14,
+                color: AppColors.black45515D,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: flip == false
+                    ? MediaQuery.of(context).size.width * 0.914667
+                    : MediaQuery.of(context).size.width * 0.78667,
+                child: TextFormField(
+                  controller: _textFieldController,
+                  decoration: InputDecoration(
+                    errorStyle: TextStyle(height: 0),
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 11.0),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: AppColors.greyD0D3D6),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: AppColors.greyD0D3D6),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: AppColors.greyD0D3D6),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    hintText: 'Enter Promo Code',
+                    hintStyle: TextStyle(
+                      color: AppColors.greyD0D3D6,
+                      fontSize: 12.0,
+                    ),
+                  ),
+                ),
+              ),
+              Visibility(
+                  visible: flip,
+                  child: IconButton(
+                      onPressed: () {
+                        _textFieldController.clear();
+                        setState(() {
+                          context.read<Studio>().removePromoCode();
+                          flip = !flip;
+                        });
+                      },
+                      icon: Icon(Icons.delete_outline)))
+            ],
+          ),
+          // Visibility(
+          //   visible: promoCode != null,
+          //   child: Padding(
+          //     padding: const EdgeInsets.only(top: 6),
+          //     child: Text(
+          //       '${promoCode?.message ?? 'Promo code applied successfully'}',
+          //       style: TextStyle(
+          //         color: AppColors.green22D896,
+          //         fontSize: 14,
+          //         fontWeight: FontWeight.w800,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          Visibility(
+            visible: !flip,
+            child: FilledButtonWidget(
+              // title: promoCode == null ? 'Apply' : 'Remove',
+              title: 'Apply',
+              onPress: () {
+                if (_textFieldController.text.isNotEmpty) {
+                  ShowLoadingDialog(context);
+                  FocusManager.instance.primaryFocus?.unfocus();
+                  setState(() {
+                    flip = !flip;
+                  });
+                  context
+                      .read<Studio>()
+                      .applyPromoCode(_textFieldController.text.toString())
+                      .then((response) {
+                    ShowLoadingDialog(context, dismiss: true);
+                    // if (response?.statusCode != 200) {
+                    //   ShowOkDialog(
+                    //     context,
+                    //     response?.message ?? ErrorMessages.somethingWrong,
+                    //   );
+                    // }
+                  });
+                } else {
+                  _textFieldController.text = '';
+                  context.read<Studio>().removePromoCode();
+                }
+              },
+              type: ButtonType.generalGrey,
+              margin: const EdgeInsets.symmetric(vertical: 10.0),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
